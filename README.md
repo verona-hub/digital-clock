0bb060e4be78b7347a06ee666c11a0f8745a2ebd
Zoran G <68257343+verona-hub@users.noreply.github.com>
2021-06-04 21:33:57 +0100

    Changes: - joined google fonts into one link - changed github link to go to the repo - script cleanup and added comments - css cleanup

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ### This website was made with Html, Css, and Javascript. 
 
-A website that displays a digital clock with a reflection (visible on Chrome).
+A website that displays a digital clock with a reflection (not supported on Firefox).
 
 ![](https://github.com/verona-hub/digital-clock/blob/master/img/screenshot.png)
--- script.js
+++ script.js
@@ -1,41 +1,36 @@
-// Date
-(
-  function date() {
-    const date = new Date();
-    document.getElementById("date").innerHTML = date.toLocaleDateString(
-      'en-GB', {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-      }
-    );
-  }
-)
-();
+// Variables declarations
+const dateText = document.getElementById("date");
+const hourText = document.getElementById('hours');
+const minutesText = document.getElementById('minutes');
+const secondsText = document.getElementById('seconds');
 
+// Self executing date function
+(function date() {
+  // Create a new date variable
+  const date = new Date();
+  // Set the date variable output to the displayed text
+  dateText.innerHTML = date.toLocaleDateString(
+    'en-GB', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    }
+  );
+})();
 
-// Time 
-function time() {
+// Time function
+const time = () => {
+  // Get current hour, minutes, and seconds and put the value in each variable
+  const hour = new Date().getHours();
+  const minutes = new Date().getMinutes();
+  const seconds = new Date().getSeconds();
 
-  const hours = document.getElementById('hours');
-  const minutes = document.getElementById('minutes');
-  const seconds = document.getElementById('seconds');
-
-
-  const getHours = new Date().getHours();
-  const getMinutes = new Date().getMinutes();
-  const getSeconds = new Date().getSeconds();
-
-
-  hours.innerHTML = getHours;
-  minutes.innerHTML = getMinutes;
-  seconds.innerHTML = getSeconds;
-
+  // Set the text to be the variables value
+  hourText.innerHTML = hour;
+  minutesText.innerHTML = minutes;
+  secondsText.innerHTML = seconds;
 }
 
+// The time functions runs at interval of 1s
 const interval = setInterval(time, 1000);
-
-
-
-
